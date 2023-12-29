238a5ae60bf943d33520c88180152b5e8f256564
Sanket Pathare <126545815+sanket8808@users.noreply.github.com>
2023-12-29 16:41:30 +0530

    contact

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,18 +1,36 @@
 import "../components/ContactForm.css";
+import  { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function ContactForm() {
+  
+   const form = useRef();
+
+   const sendEmail = (e) => {
+     e.preventDefault();
+ 
+     emailjs.sendForm('service_segfo1f', 'template_m8rzli8', form.current, 'z4iQNKsh8tblpOQsb')
+       .then((result) => {
+           console.log(result.text);
+       }, (error) => {
+           console.log(error.text);
+       });
+   };
+
     return (
     <div className="form-container">
      <h1>
         Send a Message to Us!
      </h1>
-     <form>
-     <input placeholder="Name"/>
-     <input placeholder="Email"/>  
-     <input placeholder="Subject"/>
-     <textarea placeholder="Message" rows="4"></textarea>
-     <button>Send Message</button>
-     </form>
+     <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+     <input type="text" name="to_name" required/>
+     <label>Email</label>
+     <input type="email" name="from_email" required/>  
+     <label>Message</label>
+      <input name="message"  rows="4" required/>
+     <button aria-required type="submit">Send Message </button>
+    </form>
     </div>
     );
   }
